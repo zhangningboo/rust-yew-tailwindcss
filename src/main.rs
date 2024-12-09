e479731efb173ffe5cd2c35a0bf86abea4d603c0
zhangningboo <zhangningbo21@mails.ucas.ac.cn>
2024-12-09 23:53:41 +0800

    feat: 代码备份

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ fn App() -> Html {
             <h1 class="text-base/7 font-semibold text-zinc-950 sm:text-sm/6 dark:text-white">
                 {"Hello World"}
             </h1>
-            <button { onclick } class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"> { "Click +1" } </button>
+            <button { onclick } class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 hover:bg-sky-700"> { "Click +1" } </button>
             <p> { *counter } </p>
             <table class="border-separate border border-green-800">
                 <thead>
                     <tr>
-                    <th class="border border-green-600">{ "State" }</th>
+                    <th class=" border border-green-600">{ "State" }</th>
                     <th class="border border-green-600">{ "City" }</th>
                     </tr>
                 </thead>
@@ -34,22 +34,6 @@ fn App() -> Html {
             </table>
         </div>
     }
-    // html! {
-    //     <table class="border-separate border border-green-800">
-    //         <thead>
-    //             <tr>
-    //             <th class="border border-green-600">{ "State" }</th>
-    //             <th class="border border-green-600">{ "City" }</th>
-    //             </tr>
-    //         </thead>
-    //         <tbody>
-    //             <tr>
-    //             <td class="border border-green-600">{ "Indiana" }</td>
-    //             <td class="border border-green-600">{ "Indianapolis" }</td>
-    //             </tr>
-    //         </tbody>
-    //     </table>
-    // }
 }
 
 fn main() {
